974c2cf281af63b4e5618d5d797eb868c5470320
loli-tiger <l-lu19@tsinghua.org.cn>
2025-06-27 13:45:13 +0800

    Update app.js

--- threejs/app.js
+++ threejs/app.js
@@ -7,7 +7,7 @@ export default function initApp(THREE) {
     const h = window.innerHeight
     
     const scene = new THREE.Scene()
-
+    //创建物体
     const axes = new THREE.AxesHelper(2, 2, 2)
     scene.add(axes)
 
@@ -15,14 +15,20 @@ export default function initApp(THREE) {
     const material = new THREE.MeshBasicMaterial()
     const cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
-    
+    //位置
+    cube.position.set(1, 1, 1)
+    //旋转
+    cube.rotation.x = 45 / 180 * Math.PI
+    cube.rotation.y = 45 / 180 * Math.PI
+    //缩放
+    //创建灯光
     const light = new THREE.AmbientLight()
     scene.add(light)
-    
+    //创建相机
     const camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 100);
     camera.position.set(5, 5, 5)
     camera.lookAt(0,0,0)
-    
+    //创建渲染器
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(w,h)
     renderer.render(scene,camera)
